86ec8ef075cd99bd1b8b4dfe07fac2eb71e7b871
Vinícius <viniciusdeab@outlook.com>
2022-06-07 13:26:44 -0300

    Adicionando função para adicionar a classe 'ativa' quando tecla for acionada por uma tecla

--- aluramidi-curso/main.js
+++ aluramidi-curso/main.js
@@ -3,7 +3,7 @@ function tocaSom (idElementoAudio) {
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-
+/*
 let contador = 0;
 
 while (contador < listaDeTeclas.length) {
@@ -18,4 +18,26 @@ while (contador < listaDeTeclas.length) {
     };
 
     contador = contador + 1;
+}
+*/
+for(i = 0; i < listaDeTeclas.length; i++){
+
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+
+    tecla.onkeydown = function () {
+        if(evento.code === "Enter" || evento.code === "Space"){
+        tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
 }

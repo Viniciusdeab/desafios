77eaef58dc965f5421e24b60192b81acdfccfabe
Vinícius <viniciusdeab@outlook.com>
2022-06-07 16:04:29 -0300

    Finalização e melhoria do codigo, e adição do aviso caso acha um erro de som do elemento

--- aluramidi-curso/main.js
+++ aluramidi-curso/main.js
@@ -1,5 +1,18 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+    /*
+    if(elemento === null){
+        Formas de apresentar o erro para o usuario
+        alert('Elemento nçao encontrado');
+        console.log('Elemento não encontrado');
+        console.error('Elemento não encontrado');
+    } 
+    */
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.error('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -20,19 +33,19 @@ while (contador < listaDeTeclas.length) {
     contador = contador + 1;
 }
 */
-for(i = 0; i < listaDeTeclas.length; i++){
+for (i = 0; i < listaDeTeclas.length; i++) {
 
     const tecla = listaDeTeclas[i];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
-    
+
     tecla.onclick = function () {
         tocaSom(idAudio);
     };
 
-    tecla.onkeydown = function () {
-        if(evento.code === "Enter" || evento.code === "Space"){
-        tecla.classList.add('ativa');
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Enter' || evento.code === 'Space') {
+            tecla.classList.add('ativa');
         }
     }
 

37b5b7d27efd6d52c083d37f275a1c640990163e
Vinícius <viniciusdeab@outlook.com>
2022-06-10 23:51:14 -0300

    Adição da dinamica, para aparecer as mensagens quando clicar em buscar e no icone de mensagem

--- mensageBarSearch/script.js
+++ mensageBarSearch/script.js
@@ -1,7 +1,16 @@
-$('#search').focusin(function () {
-    $('Modal').show();
+let menu = document.querySelector('.btn-menu');
+menu.onclick = function(){
+    menu.classList.toggle("ativo");
+}
+
+$("#search").focusin(function () {
+    $("#Modal").show();
 });
 
-$('#close').click(function () {
-    $('Modal').hide();
+$("#close").click(function () {
+    $("#Modal").hide();
+});
+
+$(".btn-menu").click(function () {
+    $("#Modal").show();
 });

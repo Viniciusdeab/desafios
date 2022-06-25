349945e69d2387c73caa1e48a620d66db0040d7f
Vinícius <viniciusdeab@outlook.com>
2022-06-25 14:57:39 -0300

    Adição da dinamica do relogio para atualizar de acordo com o horario atual

---
+++ digitalClock/main.js
@@ -0,0 +1,33 @@
+(()=>{
+
+    setInterval(() => {
+                
+        let hours = document.getElementById('hours');
+        let minutes = document.getElementById('minutes');
+        let seconds = document.getElementById('seconds');
+        let ampm = document.getElementById('ampm');
+
+        let h = new Date().getHours();
+        let m = new Date().getMinutes();
+        let s = new Date().getSeconds();
+        let am = h >=12 ? "PM" : "AM";
+
+        // converter 24hrs para 12hrs;
+
+        if(h > 12){
+            h = h - 12;
+        }
+
+        //adicionando zero antes do numero;
+        h = (h < 10) ? "0" + h : h;
+        m = (m < 10) ? "0" + m : m;
+        s = (s < 10) ? "0" + s : s;
+
+        hours.innerHTML = h;
+        minutes.innerHTML = m;
+        seconds.innerHTML = s;
+        ampm.innerHTML = am;
+
+    });
+
+})();

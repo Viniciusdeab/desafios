7a53f78872ff80b0a2b56df3234249fe5eac1a4a
Vinícius <viniciusdeab@outlook.com>
2022-08-05 13:04:03 -0300

    Adiação da funcionalidade de verificar se já existe o item no localStorage e se tiver atualizar ele

--- mochila-de-viagem-main/js/app.js
+++ mochila-de-viagem-main/js/app.js
@@ -2,40 +2,59 @@ const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
 const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
-itens.forEach((element) =>{
-    console.log(element.nome, element.quantidade);
+itens.forEach((element) => {
+    criaElemento(element);
 })
 
-form.addEventListener("submit", (evento)=> {
+form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
     const nome = evento.target.elements['nome'];
     const quantidade = evento.target.elements['quantidade'];
 
-    criaElemento(nome.value, quantidade.value);
+    const existe = itens.find(element => element.nome === nome.value);
+
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    if (existe) {
+        itemAtual.id = existe.id;
+
+        atualizaElemento(itemAtual);
+
+        itens[existe.id] = itemAtual;
+
+    } else {
+        itemAtual.id = itens.length;
+
+        criaElemento(itemAtual);
+
+        itens.push(itemAtual);
+    }
+
+    localStorage.setItem("itens", JSON.stringify(itens));
 
     nome.value = "";
-    quantidade.value ="";
+    quantidade.value = "";
 });
 
-function criaElemento(nome, quantidade){
+function criaElemento(item) {
     const novoItem = document.createElement("li");
     novoItem.classList.add("item");
 
     const numeroItem = document.createElement("strong");
-    numeroItem.innerHTML = quantidade;
+    numeroItem.innerHTML = item.quantidade;
+    numeroItem.dataset.id = item.id
 
     novoItem.appendChild(numeroItem);
-    novoItem.innerHTML += nome;
+    novoItem.innerHTML += item.nome;
 
     lista.appendChild(novoItem);
 
-    const itemAtual = {
-        nome: nome,
-        quantidade: quantidade
-    }
+}
 
-    itens.push(itemAtual);
-
-    localStorage.setItem("itens", JSON.stringify(itens));
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
 }
